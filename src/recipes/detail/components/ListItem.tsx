4493e80d6aa50859cdf498349b0bf4a33de78ea1
smit153 <smitsojitra332003@gmail.com>
2024-11-14 21:31:00 +0530

    fix: add handling for whitespace in list items

--- src/recipes/detail/components/ListItem.tsx
+++ src/recipes/detail/components/ListItem.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ListItem: React.FC<{ value: string, index: number }> = ({ value, index }) => {
   return (
     <li key={index} className="text-leaf-green-800 dark:text-gray-200 p-2">
-      {value}
+      <span className="max-w-full break-word whitespace-pre-wrap">{value}</span>
     </li>
   );
 };
